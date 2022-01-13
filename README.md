05bf3463fa7c420915939863383e65b97a11dfcf
Alvaro <alvaroriba585@gmail.com>
2022-01-13 09:56:49 -0300

    Second Commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# calculadora.py
-Calculadora en Python
+# Calculadora de Python
+
+---
+
+Aquí está el codigo de como crear una calculadora facil y rapido en Python (Funciona en consola)
+
+> Descarga Python [Aquí](https://www.python.org/downloads)
+
+###### Hecho por focamarina
---
+++ calc.py
@@ -0,0 +1,37 @@
+n1 = float(input("Introduce tu primer numero: "))
+n2 = float(input("Introduce tu segundo numero: "))
+
+option = 0
+
+while True:
+    print("""
+        Dime, ¿que operacion quieres realizar?
+
+        1) Sumar los dos numeros
+        2) Restar los dos numeros
+        3) Multiplicar los dos numeros
+        4) Dividir los dos numeros
+        5) Cambiar los numeros elegidos
+        6) Cancelar operacion
+    """)
+    option = int(input("Introduzca el numero de la opcion a realizar: "))
+
+    if option == 1:
+        print(" ")
+        print(f"La suma de {n1} y {n2} da como resultado: ",n1+n2)
+    elif option == 2:
+        print(" ")
+        print(f"La resta de {n1} y {n2} da como resultado: ",n1-n2)
+    elif option == 3:
+        print(" ")
+        print(f"La multiplicacion de {n1} y {n2} da como resultado: ",n1*n2)
+    elif option == 4:
+        print(" ")
+        print(f"La division de {n1} y {n2} da como resultado: ",n1/n2)
+    elif option == 5:
+        n1 = float(input("Introduce tu primer numero: "))
+        n2 = float(input("Introduce tu segundo numero: "))
+    elif option == 6:
+        break
+    else:
+        print("Opcion Incorrecta!")
